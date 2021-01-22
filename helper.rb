59b690128204eb4f05eb9cb6ebfefbc9cd80c131
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-01-21 21:01:44 -0800

    Add comments

--- helper.rb
+++ helper.rb
@@ -12,16 +12,6 @@ def get_value_from_file (file, key)
     return (value.nil? || value.empty?) ? nil : value 
 end
 
-def write_to_sender_log (log_file, num_of_attempts, last_attempt_time)
-    File.open(log_file, 'w') { |file|
-        info = [
-            "CURRENT_ATTEMPTS=#{num_of_attempts}",
-            "LAST_ATTEMPT_TIME=#{CURRENT_TIME}"
-        ]
-        file.write(info.join("\n"))
-    }
-end
-
 def update_value_in_file(file, key, new_value) 
     system("sed -i \'s/#{key}=.*/#{key}=#{new_value}/g\' #{file}")
 end
@@ -30,13 +20,13 @@ def remove_whitespace(string)
     return string.to_s.gsub(/\s+/,"")
 end
 
-def get_log_line_number(time)
+def get_current_time_linenum_in_log(time)
     log_bookmark = get_value_from_file(CONFIG_FILE, "LOG_BOOKMARK").gsub(/\s+/,"").to_i
     if log_bookmark >= 1
-        line_offset = `tail -n +#{log_bookmark} #{AUTH_LOG} | awk '/#{CURRENT_TIME}/{ print NR; exit }'`
+        line_offset = `tail -n +#{log_bookmark} #{AUTH_LOG} | awk '/#{time}/{ print NR; exit }'`
         log_line_number = log_bookmark + line_offset.to_i
     else
-        log_line_number = `awk \'/#{CURRENT_TIME}/{ print NR; exit }\' #{AUTH_LOG}`
+        log_line_number = `awk \'/#{time}/{ print NR; exit }\' #{AUTH_LOG}`
     end
 
     log_line_number = remove_whitespace(log_line_number)
@@ -48,12 +38,6 @@ def append_to_file(filename, line)
     File.open(filename, 'a+') { |file|
         file.puts line
     }
-end
-
-def get_current_fail_line
-    starting_line = get_log_line_number()
-    auth_log_line = `tail -n +#{starting_line} #{AUTH_LOG} | grep -a -m 1 -h 'Failed password'`
-    return auth_log_line
 end
 
 def extract_ip_from_line(line)
--- ssh-logger.rb
+++ ssh-logger.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 require 'fileutils'
 require_relative "helper.rb"
 
@@ -7,22 +8,40 @@ CURRENT_TIME = "Jan 21 07:15:03"
 
 ENABLE_SSH_MONITOR = get_value_from_file(CONFIG_FILE, "ENABLE_SSH_MONITOR").freeze
 MAX_ATTEMPTS = get_value_from_file(CONFIG_FILE, "MAX_ATTEMPTS").to_i.freeze
-LOCK_TIME = get_value_from_file(CONFIG_FILE, "LOCK_TIME").freeze
+LOCK_TIME = get_value_from_file(CONFIG_FILE, "LOCK_TIME").to_i.freeze
+
+def get_current_fail_line
+    starting_line = get_current_time_linenum_in_log(CURRENT_TIME)
+    auth_log_line = `tail -n +#{starting_line} #{AUTH_LOG} | grep -a -m 1 -h 'Failed password'`
+    return auth_log_line
+end
+
+def create_sender_logfile (log_file, num_of_attempts, last_attempt_time)
+    File.open(log_file, 'w') { |file|
+        info = [
+            "CURRENT_ATTEMPTS=#{num_of_attempts}",
+            "LAST_ATTEMPT_TIME=#{CURRENT_TIME}"
+        ]
+        file.write(info.join("\n"))
+    }
+end
 
 if ENABLE_SSH_MONITOR == "true"
-    current_fail_line = get_current_fail_line()
 
-    append_to_file(FAILED_LOG, current_fail_line)
+    current_fail_line = get_current_fail_line() # Get the log line for current failed attempt
     sender_ip = extract_ip_from_line(current_fail_line)
 
+    # Save number of login attempts and time of last attempt of a specific IP to a textfile named <ipaddr>"
+    # Save the textfile for each IP to a single directory
     sender_logs = "#{IP_LOGS_DIR}/#{sender_ip}"
     FileUtils.mkdir(IP_LOGS_DIR) unless Dir.exists?(IP_LOGS_DIR)
+    
     if File.exists?(sender_logs)
         current_attempts = get_value_from_file(sender_logs, "CURRENT_ATTEMPTS").to_i
         last_attempt_time = get_value_from_file(sender_logs, "LAST_ATTEMPT_TIME")
 
         if current_attempts >= MAX_ATTEMPTS
-            puts "BLOCK IP"
+            puts "BLOCK IP" #TODO: add firewall block scripts here
             exit(1)
         else
             current_attempts += 1
@@ -30,7 +49,7 @@ if ENABLE_SSH_MONITOR == "true"
             update_value_in_file(sender_logs, "LAST_ATTEMPT_TIME", CURRENT_TIME) 
         end
     else
-        write_to_sender_log(sender_logs, 1, CURRENT_TIME)
+        create_sender_logfile(sender_logs, 1, CURRENT_TIME)
     end
 else
     exit(0)
