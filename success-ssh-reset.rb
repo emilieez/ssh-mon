0546bac326d366b49982f0e8022ce32d046f91e4
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-01-23 19:05:18 -0800

    Add missing bracket

--- success-ssh-reset.rb
+++ success-ssh-reset.rb
@@ -3,6 +3,6 @@ require_relative "lib/presets.rb"
 
 ssh_log_file = "#{IP_LOGS_DIR}/#{ENV['PAM_RHOST']}"
 if File.exists?(ssh_log_file)
-    system("sed -i 's/CURRENT_ATTEMPTS=.*/CURRENT_ATTEMPTS=0/g' #{ssh_log_file}"
+    system("sed -i 's/CURRENT_ATTEMPTS=.*/CURRENT_ATTEMPTS=0/g' #{ssh_log_file}")
 end
 exit(0)
