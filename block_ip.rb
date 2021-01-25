2b9eaca2f0aeb49036be4a1f7da8e3dd0b8c5631
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-01-24 23:01:57 -0800

    Remove save

--- block_ip.rb
+++ block_ip.rb
@@ -7,11 +7,9 @@ LOCK_TIME = get_value_from_file(CONFIG_FILE, "LOCK_TIME").to_i.freeze
 lock_time_seconds = LOCK_TIME * 60
 
 system("sudo iptables -A INPUT -s #{sender_ip} -j DROP")
-system("sudo service iptables save")
 
 sleep(lock_time_seconds)
 
-system("sudo iptables -D INPUT -s #{sender_ip}-j DROP")
-system("sudo service iptables save")
+system("sudo iptables -D INPUT -s #{sender_ip} -j DROP")
 
 exit(0)
