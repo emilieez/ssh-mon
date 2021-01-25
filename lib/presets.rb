711e9c232f5d08a4e64bcb4a564948f206aa135a
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-01-24 16:34:44 -0800

    Fix log dir typo

--- lib/presets.rb
+++ lib/presets.rb
@@ -2,6 +2,6 @@ DEFAULT_MAX_ATTEMPT = 3
 DEFAULT_LOCK_TIME = 5
 
 CONFIG_FILE = "ssh_mon_config"
-AUTH_LOG = "/var/logs/auth.log"
+AUTH_LOG = "/var/log/auth.log"
 FAILED_LOG = "failed_auth.log"
-IP_LOGS_DIR = "/var/logs/ip_logs"
+IP_LOGS_DIR = "/var/log/ip_logs"
