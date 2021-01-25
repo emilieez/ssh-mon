55bac132a51f3c3dbc911194a3c9065a73e326bf
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-01-24 22:55:23 -0800

    Add sudo

--- block_ip.rb
+++ block_ip.rb
@@ -6,12 +6,12 @@ sender_ip = ARGV[0]
 LOCK_TIME = get_value_from_file(CONFIG_FILE, "LOCK_TIME").to_i.freeze
 lock_time_seconds = LOCK_TIME * 60
 
-system("iptables -A INPUT -s #{sender_ip} -j DROP")
-system("service iptables save")
+system("sudo iptables -A INPUT -s #{sender_ip} -j DROP")
+system("sudo service iptables save")
 
 sleep(lock_time_seconds)
 
-system("iptables -D INPUT -s #{sender_ip}-j DROP")
-system("service iptables save")
+system("sudo iptables -D INPUT -s #{sender_ip}-j DROP")
+system("sudo service iptables save")
 
 exit(0)
