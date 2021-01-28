c06a97332003232d5a9e491b8383c73c8e625afe
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-01-27 19:46:26 -0800

    Remove log file on success

--- pam/success-ssh-reset.rb
+++ pam/success-ssh-reset.rb
@@ -3,7 +3,5 @@ require_relative "../lib/presets.rb"
 require_relative "../lib/texts_helper.rb"
 
 ssh_log_file = "#{IP_LOGS_DIR}/#{ENV['PAM_RHOST']}"
-if File.exists?(ssh_log_file)
-    update_value_in_file(ssh_log_file, CURRENT_ATTEMPTS, 0) 
-end
+system("sudo rm #{ssh_log_file}") if File.exists?(ssh_log_file)
 exit(0)
